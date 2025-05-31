577d96972861be7b5264bbfcfd2e43494dc873d6
shreyasprabhakar <shrprabh@ttu.edu>
2025-05-31 13:36:05 -0500

    Implement chat component layout and styles, including user info and action icons

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,7 +1,29 @@
 import "./chat.css";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"> </div>
+    </div>
+  );
 };
 
 export default Chat;
